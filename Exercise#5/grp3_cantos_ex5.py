3986091eabee26eb0372372e61b7fb2847f43d69
JohnRickCantos <168394396+JohnRickCantos@users.noreply.github.com>
2024-04-29 12:35:49 +0800

    Add files via upload

---
+++ Exercise#5/grp3_cantos_ex5.py
@@ -0,0 +1,25 @@
+import math
+#Number functions
+example_x = 3.14
+example_y = math.ceil(example_x) # round a number
+print(example_y, "\n")
+
+#Power & logarithmic functions
+example1_x = 100
+example2_y = math.log10(example1_x) # calculate the base 10 logarithmic of a number
+print(example2_y, "\n")
+
+#Trigometric functions
+example3_x = math.pi / 2 # 90 degrees radians
+example4_y = math.sin(example3_x) # calculate sin of 90 degrees
+print(example4_y, "\n")
+
+#Angular conversion functions
+example5_x = 180 # degrees
+example6_y = math.radians(example5_x) #convert 180 degrees to radians
+print (example6_y, "\n") 
+
+#Hyperbolic functions
+example7_x = 0 
+example8_y = math.cosh(example7_x) # Calculate the hyperbolic cosine of 0
+print(example8_y, "\n")
